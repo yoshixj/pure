115b9e86538205049edb6f364b8eb00f25fd2130
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2018-05-06 13:52:19 +0900

    change-color

--- pure.zsh
+++ pure.zsh
@@ -125,7 +125,7 @@ prompt_pure_preprompt_render() {
 	local -a preprompt_parts
 
 	# Set the path.
-	preprompt_parts+=('%F{blue}%~%f')
+	preprompt_parts+=('%F{cyan}%~%f')
 
 	# Add git branch and dirty status info.
 	typeset -gA prompt_pure_vcs_info
